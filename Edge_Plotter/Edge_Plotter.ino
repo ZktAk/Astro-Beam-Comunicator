4348c9dbfa598d5e83ee8478680d59c4182a3a2a
Zachary Donaldson <95774165+ZktAk@users.noreply.github.com>
2025-10-23 20:47:23 -0400

    Add temporal response characterization code

---
+++ Edge_Plotter/Edge_Plotter.ino
@@ -0,0 +1,34 @@
+// Reciever code
+const int analogPin = A0;             // Photodiode input
+
+unsigned long raws[200];
+
+bool printed = 0;
+int n = 0;
+
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  int raw = analogRead(analogPin); 
+
+  if (raw > 25 and raw < 930) {
+    raws[n] = raw;
+    n++;
+    printed = 1;
+  }
+
+  else if (printed){
+    for (int i = 0; i < n; i++){
+      Serial.println(raws[i]);
+    }
+    n=0;  
+    printed = 0;
+  }
+
+  delayMicroseconds(100);
+
+
+}
